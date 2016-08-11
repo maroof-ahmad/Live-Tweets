837e5ab863fb841569d1db5ea98117554d8b8236
Maroof Ahmad <ahmadmaroof8@gmail.com>
2016-08-11 18:46:22 +0530

    config

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   twitter : {
-    consumer_key: 'SN7u5FVFb54eqhpazxVfiLc3h',
-    consumer_secret: 'qiKO5Ilywz3HG6MmQJDNvZmUbyjNcFKh7Fp8Ks8EIlsJSBoomP',
-    access_token_key: '2245755504-ZNOGsdSFQLuGwb6439ujz2SnW15m2aVXet02jjy',
-    access_token_secret: 'MmnOhas8jEz8XA3usj3LsXbuWiosoF1E4RnjpeCKSQzXt'
+    consumer_key: '',
+    consumer_secret: '',
+    access_token_key: '',
+    access_token_secret: ''
   }
 }
